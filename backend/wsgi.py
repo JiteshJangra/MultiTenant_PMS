247f9467d64a9962c53c54ac465dc583afbcfa67
JiteshJangra <jitesh5921@gmail.com>
2025-08-28 10:38:29 +0530

    library fix

--- backend/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Replace 'settings' with your actual settings module name if different
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
-application = get_wsgi_application()
